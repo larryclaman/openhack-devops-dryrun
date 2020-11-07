36e9a0bbd3e301fa87a9c3bcda5aab31000e1fc9
Larry Claman <25161683+larryclaman@users.noreply.github.com>
2020-11-07 16:26:35 -0500

    testswap.sh

---
+++ apis/poi/testswap.sh
@@ -0,0 +1,5 @@
+RG="openhack8pm8c3y5rg"
+AZURE_WEBAPP_NAME="openhack8pm8c3y5userprofile"   # set this to your application's name
+
+az webapp deployment slot swap -g $RG -n $AZURE_WEBAPP_NAME  \
+             --slot staging  --target-slot production
